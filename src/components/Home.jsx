efe01b5758a22a6dccd45f212b8972a35aea0cf5
Radmir <radmir_96@mail.ru>
2022-10-11 17:13:55 +0500

    .env & bg-image bug fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,14 +10,13 @@ const Home = () => {
     const [weather, setWeather] = useState({})
     const [loading, setLoading] = useState(false)
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=5411cb8f11258d8ff81cf6d24d7dc13e`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
 
     const fetchWeather = (e) => {
         e.preventDefault()
         setLoading(true)
         axios.get(url).then((response) => {
             setWeather(response.data)
-            // console.log(response.data)
         })
         setCity('')
         setLoading(false)
@@ -28,21 +27,23 @@ const Home = () => {
     } else {
         return (
             <>
-                {/* overlay */}
-                <div className='bg-black/40 absolute top-0 left-0 right-0 bottom-0 z-[1]'></div>
-                {/* search */}
-                <div className='relative flex jusify-between items-center max-w-[500px] w-full pt-4 px-4 m-auto text-white z-10'>
-                    <form onSubmit={fetchWeather} className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 rounded-2xl'>
-                        <div>
-                            <input
-                                onChange={(e) => setCity(e.target.value)}
-                                className='bg-transparent border-none text-white focus:outline-none text-2xl capitalize' type="text" placeholder='Search city' />
-                        </div>
-                        <button onClick={fetchWeather}><BsSearch size={20} /></button>
-                    </form>
+                <div className='bg-[url(https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1550&q=80)] w-full h-screen bg-no-repeat bg-center bg-cover'>
+                    {/* overlay */}
+                    <div className='bg-black/40 absolute top-0 left-0 right-0 bottom-0 z-[1]'></div>
+                    {/* search */}
+                    <div className='relative flex jusify-between items-center max-w-[500px] w-full pt-4 px-4 m-auto text-white z-10'>
+                        <form onSubmit={fetchWeather} className='flex justify-between items-center w-full m-auto p-3 bg-transparent border border-gray-300 rounded-2xl'>
+                            <div>
+                                <input
+                                    onChange={(e) => setCity(e.target.value)}
+                                    className='bg-transparent border-none text-white focus:outline-none text-2xl capitalize' type="text" placeholder='Search city' />
+                            </div>
+                            <button onClick={fetchWeather}><BsSearch size={20} /></button>
+                        </form>
+                    </div>
+                    {/* weather */}
+                    {weather.main && <Weather data={weather} />}
                 </div>
-                {/* weather */}
-                {weather.main && <Weather data={weather} />}
             </>
         )
     }
